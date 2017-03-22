93500025bf30895060471b07ad82afd7b55ab1a6
fudd <5036175@qq.com>
2017-03-22 16:42:59 +0800

    sync

--- app/src/main/java/com/fudd/MainActivity.java
+++ app/src/main/java/com/fudd/MainActivity.java
@@ -1,6 +1,7 @@
 package com.fudd;
 
 import android.content.Intent;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
